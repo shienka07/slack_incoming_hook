9758dd767a7ad3d8d799fb9a7ed95abedf7c3ef3
Kyeonghye JO <shienka07.ko@gmail.com>
2025-02-20 17:28:32 +0900

    upgrade

--- Bot.java
+++ Bot.java
@@ -6,12 +6,14 @@ import java.util.*;
 public class Bot {
     public static void main(String[] args) { 
         String webhookUrl = System.getenv("SLACK_WEBHOOK_URL");
+        String message = System.getenv("SLACK_WEBHOOK_URL");
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
             .uri(URI.create(webhookUrl))
             .header("Content-Type", "application/json")
-            .POST(HttpRequest.BodyPublishers.ofString("{\"text\":\"테스트 메시지\"}")) 
+            // .POST(HttpRequest.BodyPublishers.ofString("{\"text\":\"테스트 메시지\"}")) 
+            .POST(HttpRequest.BodyPublishers.ofString("{\"text\":\" + " + message + "\"}")) 
             .build();
         
         try {
